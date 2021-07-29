600b52deac75c32e66d2b9f44e25367b88e15440
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-07-29 12:49:38 +0200

    2d

---
+++ exemple6/2d-perlin-noise.js
@@ -0,0 +1,22 @@
+let inc= 0.01;
+
+function setup() {
+    createCanvas(200, 200);
+    pixelDensity(1);
+}
+
+function draw() {
+    loadPixels();
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y){
+            let index = (x + y * width) * 4;
+            let r = random(255);
+            pixels[index+0] = r;
+            pixels[index+1] = r;
+            pixels[index+2] = r;
+            pixels[index+3] = r;
+
+        }
+    }
+    updatePixels();
+}
