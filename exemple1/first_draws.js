51a2de8ce0818383c7f78c930c8ce5eb0ae6aa9c
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-07-30 11:20:30 +0200

    simple exemple

---
+++ exemple1/first_draws.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(255);  
+    
+    fill(0);
+    ellipse( 200, 200, 200, 200);
+    ellipseMode(CENTER);
+    
+    line(0, 50, 400, 400); 
+  }
