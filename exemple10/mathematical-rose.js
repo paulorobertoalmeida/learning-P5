00f753583131292ddbe1f903cfd21abf7ee0a9b2
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-02 10:26:07 +0200

    mathematical rose

---
+++ exemple10/mathematical-rose.js
@@ -0,0 +1,30 @@
+let d = 8;
+let n = 5;
+let sliderD;
+let sliderN;
+
+function setup() {
+  createCanvas(400, 400);
+  sliderD = createSlider(1, 10, 5, 0.1);
+  sliderN = createSlider(1, 10, 5, 0.1);
+}
+
+function draw() {
+  d = sliderD.value();
+  n = sliderN.value();
+  let k = n/d;
+  background(50);
+  translate(width/2, height/2);
+  noFill();
+  
+  beginShape();
+  for ( let a = 0; a < TWO_PI * d; a += 0.02){
+    let r = 200 * cos(k * a);
+    let x = r * cos(a);
+    let y = r * sin(a);
+    stroke(255);
+    strokeWeight(4);
+    vertex(x, y);
+  }
+  endShape(CLOSE);
+}
