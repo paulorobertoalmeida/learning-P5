488ece9c554823bb7bc3f14a312a18400d50b06d
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-03 17:35:50 +0200

    line and point

---
+++ Introduction to P5 book/line.js
@@ -0,0 +1,12 @@
+
+function setup() {
+    createCanvas(400, 400);
+    background(10);
+    stroke(255);
+  }
+  
+  function draw() {
+    
+    strokeWeight(2);
+    line(20, 50, 320, 110);
+  }
--- Introduction to P5 book/single-point.js
+++ Introduction to P5 book/single-point.js
@@ -1,13 +1,8 @@
-
-//single point exercice
 function setup() {
     createCanvas(400, 400);
-    background(10);
-    stroke(255);
-  }
-  
-  function draw() {
-    
-    strokeWeight(2);
+}
+
+function draw() {
+    background(204);
     point(240, 60);
-  }
+}
