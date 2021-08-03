851cfe4133cc96b9506a0a4974d4e31a2d078c48
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-03 17:28:02 +0200

    single point

---
+++ Introduction to P5 book/single-point.js
@@ -0,0 +1,13 @@
+
+//single point exercice
+function setup() {
+    createCanvas(400, 400);
+    background(10);
+    stroke(255);
+  }
+  
+  function draw() {
+    
+    strokeWeight(2);
+    point(240, 60);
+  }
