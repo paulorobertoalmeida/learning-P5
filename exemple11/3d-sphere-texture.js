9e0538ef240912b730f60c95201a28bee888810b
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-03 11:02:31 +0200

    3d sphere texture

---
+++ exemple11/3d-sphere-texture.js
@@ -0,0 +1,32 @@
+//2d texture & 3d box
+let angle = 0;
+let art;
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  art = createGraphics(400, 400);
+}
+
+function draw() {
+  background(50);
+  push();
+    texture(art);
+    noStroke();
+    rotateX(angle);
+    rotateY(angle);
+    rotateZ(angle);
+    sphere(50);
+  
+  angle += 0.05;
+  pop();
+  
+  push();
+  for(let i = 50; i < width; i += 50){
+    for(let j = 50; j < height; j += 50){
+      art.noFill();
+      art.stroke(i, j, 0);
+      art.strokeWeight(2);
+      art.ellipse(i, j,50 + mouseX, 50 + mouseY);
+    }
+  }
+  pop();
+}
