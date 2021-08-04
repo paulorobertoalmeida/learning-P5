a252a2a6a558184239ec141fce9da019c116a36b
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-04 10:48:34 +0200

    drawing order

---
+++ Introduction to P5 book/drawing-order.js
@@ -0,0 +1,14 @@
+//drawing order
+function setup(){
+    createCanvas(480, 120);
+  }
+  
+  function draw(){
+    background(50);
+    
+    fill('yellow');
+    ellipse(140, 0, 190, 190);
+    
+    fill('red');
+    rect(160, 30, 260, 20);
+  }
