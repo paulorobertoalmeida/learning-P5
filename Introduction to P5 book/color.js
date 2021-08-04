96eb98d92bfa40fec4b4e65d9e335982abb89f91
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-04 11:19:38 +0200

    color experiments

---
+++ Introduction to P5 book/color.js
@@ -0,0 +1,21 @@
+
+//Color experiments
+function setup(){
+    createCanvas(480, 120);
+  }
+  
+  function draw(){
+    background(0);
+    
+    //first circle
+    fill(204);
+    ellipse( 132, 82, 200, 200);
+    
+    //second circle
+    fill(153);
+    ellipse(228, -16, 200, 200);
+    
+    //third circle
+    fill(102);
+    ellipse(268, 118, 200, 200);
+  }
