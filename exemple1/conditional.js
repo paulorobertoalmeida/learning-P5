cf7c4cacb988baec082cac20db5b5624c6f8e61f
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-04 12:24:47 +0200

    conditional statement

---
+++ exemple1/conditional.js
@@ -0,0 +1,18 @@
+
+//Color experiments
+function setup(){
+    createCanvas(600, 400);
+  }
+  
+  function draw(){
+    background(0);
+    
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    
+    if (mouseX > 300 && mouseY > 300){
+      fill(255, 0, 200);
+    }
+    ellipse(300, 200, 100, 100);
+  }
