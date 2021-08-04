5b430b4b5e6a63966c3bdf2b3c8e9dd52d6997bd
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-04 10:55:33 +0200

    stroke weight

---
+++ Introduction to P5 book/stroke-weight.js
@@ -0,0 +1,20 @@
+//Stroke Weight
+function setup(){
+    createCanvas(480, 120);
+  }
+  
+  function draw(){
+    background(204);
+    
+    
+    ellipse(75, 60, 90, 90);
+    
+    strokeWeight(8);
+    
+    
+    ellipse(175, 60, 90, 90);
+    
+    ellipse(279, 60, 90, 90);
+    strokeWeight(20);
+    ellipse(389, 60, 90, 90);
+  }
