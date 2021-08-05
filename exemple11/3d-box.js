23cfa4c6bc801cc9bf05d642a986119ceffd659a
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-05 17:48:45 +0200

    3d simple box

---
+++ exemple11/3d-box.js
@@ -0,0 +1,21 @@
+let angle = 0;
+
+let graphics;
+function setup() {
+  createCanvas(400, 300, WEBGL);
+  graphics = createGraphics(200,200);
+  graphics.background(255);
+}
+
+function draw() {
+  background(0);
+  ambientLight(100);
+  directionalLight(255, 255, 255, 0, 0, 1);
+  rotateX(angle);
+  rotateY(angle * 1.3);
+  rotateZ(angle * 0.7);
+  box(100);
+  
+  texture(graphics);
+  angle += 0.03
+}
