9cfb7441f28b5d7202f7e2a102c1beb22a28d2de
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-05 10:28:36 +0200

    arrow

---
+++ Introduction to P5 book/arrow.js
@@ -0,0 +1,20 @@
+//ARROW
+function setup() {
+    createCanvas(480, 400);
+  }
+  
+  function draw() {
+    background(50);
+    
+    beginShape();
+      vertex(180, 82);
+      vertex(207, 36);
+      vertex(214, 63);
+      vertex(407, 11);  
+      vertex(412, 30);
+      vertex(219, 82);
+      vertex(226, 109);
+    endShape(CLOSE);
+      
+      
+  }
