e1d381d2010c5d9985ff142a5084dea1ea508400
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-05 10:44:22 +0200

    simple dinosours

---
+++ Introduction to P5 book/dinosours.js
@@ -0,0 +1,43 @@
+//ARROW
+function setup() {
+    createCanvas(480, 120);
+  }
+  
+  function draw() {
+    background(0, 235, 150);
+    
+    //Right Dino
+    beginShape();
+    fill(255);
+      vertex(50, 120);
+      vertex(100, 90);
+      vertex(110, 60);
+      vertex(80, 10);  
+      vertex(210, 60);
+      vertex(160, 80);
+      vertex(200, 90);
+      vertex(140, 100);
+      vertex(130, 120);
+    endShape();
+    
+    fill(50);
+    ellipse(150, 60, 8, 8);
+    
+    //Left Dino
+    beginShape();
+    fill(255);
+      vertex(370, 120);
+      vertex(360, 90);
+      vertex(290, 80);
+      vertex(340, 70);  
+      vertex(280, 50);
+      vertex(420, 10);
+      vertex(390, 50);
+      vertex(410, 90);
+      vertex(460, 120);
+    endShape();
+    
+    fill(50);
+    ellipse(345, 50, 10, 10);
+      
+  }
