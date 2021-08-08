e020aedf06ff7eb37f93004f071e6f11c256e0cc
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-08 12:52:24 +0200

    spirograph last part

--- coding project/spirograph.js
+++ coding project/spirograph.js
@@ -27,15 +27,19 @@ function draw() {
   translate(width /2, height /2);
   stroke(255);
   
-  for (let i = 0; i < 5; i++){
+  for (let i = 0; i < 50; i++){
     
   let x1 = r1 * cos(a1);
   let y1 = r1 * sin(a1);
   
   let x2 = x1 + r2 * cos(a2);
   let y2 = y1 + r2 * sin(a2);
+    
+  let r = map(sin(frameCount), -1, 1, 100, 200);
+  let g = map(cos(frameCount), -1, 1, 100, 200);
+  let b = map(sin(frameCount), -1, 1, 200, 100);
           
-      
+  stroke(r, g, b);    
   line(prevX, prevY, x2, y2); 
   
   prevX = x2;
