d5b26a9e14dc639e6bb65ceeb0a257003b47b456
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-10 10:31:05 +0200

    for loop 1

---
+++ Introduction to P5 book/Folder 2/for-loop.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(480, 120);
+    strokeWeight(8);
+  }
+  
+  function draw() {
+    background(204);
+    
+    for ( let i = 20; i < 400; i += 60) {
+      line( i, 40, i + 60, 80);
+    }
+  }
