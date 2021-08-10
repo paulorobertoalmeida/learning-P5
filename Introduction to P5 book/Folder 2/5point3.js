48d38e548c71a4cb0a611e23adb9db4fa02c1ad9
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-10 17:43:19 +0200

    5.3 exemple

---
+++ Introduction to P5 book/Folder 2/5point3.js
@@ -0,0 +1,15 @@
+let x = 280;
+let y = -100;
+let diameter = 380;
+
+function setup() {
+  createCanvas(480, 120);
+  fill(102);
+ 
+}
+
+function draw() {
+  background(204);
+  ellipse(x, y, diameter, diameter);
+  
+}
