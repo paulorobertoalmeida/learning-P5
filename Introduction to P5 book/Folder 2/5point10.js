6b40e826f5ae119c71dd673c653223518f585bf7
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-11 17:37:52 +0200

    5.10 exemple

---
+++ Introduction to P5 book/Folder 2/5point10.js
@@ -0,0 +1,16 @@
+
+
+function setup() {
+  createCanvas(400, 400);
+  strokeWeight(30);
+}
+
+function draw() {
+  background(204);
+  stroke(102);
+  line(40, 0, 70, height);
+  if(mouseIsPressed == true) {
+    stroke(0);
+  }
+  line(0, 70,width, 50);
+}
