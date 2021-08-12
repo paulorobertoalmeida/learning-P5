6f9ae86c11aed093ff59e7f94cf64a09d6fd0577
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-12 16:57:12 +0200

    5.19 exemple: Arrows

---
+++ Introduction to P5 book/Folder 2/5point19.js
@@ -0,0 +1,19 @@
+//Finding specific letters
+
+let x = 215;
+
+function setup() {
+  createCanvas(480, 120);
+}
+
+function draw() {
+  if (keyIsPressed) {
+    if (keyCode == LEFT_ARROW) {
+      x--;
+    }
+    else if (keyCode == RIGHT_ARROW) {
+      x++;
+    }
+  }
+  rect(x, 45, 50, 50);
+}
