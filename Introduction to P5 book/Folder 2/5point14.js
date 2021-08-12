206c1efc7ae9686c0d26ce8b8776ea63cbb45281
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-12 10:53:46 +0200

    5.14 exemple: expanding ellipse

---
+++ Introduction to P5 book/Folder 2/5point14.js
@@ -0,0 +1,22 @@
+//Ellipse border: Testing the pointer
+
+let x = 120;
+let y = 60;
+let radio = 12;
+
+function setup() {
+  createCanvas(400, 400);
+  ellipse(RADIUS);
+}
+
+function draw() {
+  background(220);
+  let d = dist(mouseX, mouseY, x, y);
+  if (d < radio) {
+    radio++;
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(x, y, radio, radio);
+}
