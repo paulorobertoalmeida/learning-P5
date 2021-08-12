ed4dd4309e6d4e435349ddb4ccad8e5cf85cf5b0
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-12 12:27:19 +0200

    5.16 exemple: when you press a key an line appears in the screen

---
+++ Introduction to P5 book/Folder 2/5point16.js
@@ -0,0 +1,14 @@
+//Pressed key
+
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(204);
+    line(20, 20, 220, 100);
+    if (keyIsPressed) {
+      line(220, 20, 20, 100);
+    }
+  }
