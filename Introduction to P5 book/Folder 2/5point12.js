389d012fdd7e6c506c2d395e1e4efe24577c8e84
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-12 10:40:20 +0200

    5.12 exemple: interacting with multiple buttons

---
+++ Introduction to P5 book/Folder 2/5point12.js
@@ -0,0 +1,20 @@
+//Intereacting with multiple mouse button
+
+function setup() {
+    createCanvas(400, 400);
+    strokeWeight(30);
+  }
+  
+  function draw() {
+    background(204);
+    stroke(102);
+    line(40, 0, 70, height);
+    if (mouseIsPressed) {
+      if (mouseButton == LEFT){
+        stroke(255);
+        } else {
+        stroke(0);
+        }
+    line(0, 70, width, 50);
+    }
+  }
