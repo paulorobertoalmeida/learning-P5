2f4192be0adb0e1c41c187d30b2b2576f0f3fe28
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-16 10:21:41 +0200

    scaling a square

---
+++ Introduction to P5 book/Folder 3/scaling.js
@@ -0,0 +1,29 @@
+//Scale #1
+
+function setup() {
+    createCanvas(400, 400);
+   
+}
+  
+function draw() {
+  background(294);
+  
+  scale(1.5);
+  rect(20, 20, 20, 40);
+     
+}
+
+//Scale #2
+
+function setup() {
+    createCanvas(400, 400);
+   
+}
+  
+function draw() {
+  background(294);
+  
+  scale(3);
+  rect(20, 20, 20, 40);
+     
+}
