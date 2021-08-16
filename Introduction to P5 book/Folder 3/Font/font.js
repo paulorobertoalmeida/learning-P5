408131b270502575307041a3ae7852671753c18e
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-16 12:02:19 +0200

    drawing fonts

---
+++ Introduction to P5 book/Folder 3/Font/font.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(480, 400);
+    textFont("Arial");
+  }
+  
+  function draw() {
+    background(102);
+    textSize(32);
+    text("Hello Cowboy", 25, 60);
+    textSize(16);
+    text("Hello Cowboy", 27, 90);
+  }
