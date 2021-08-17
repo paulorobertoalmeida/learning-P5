f21e451b8263a1cf08694e9158e8becf7db28774
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-17 10:24:54 +0200

    Using google fonts reference

---
+++ Introduction to P5 book/Folder 3/Font/font-web.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(400, 400);
+    textFont("Source Code Pro");
+    
+  }
+  
+  function draw() {
+    background(220);
+    textSize(28);
+    text("Here comes the Cowboy", 25, 60);
+  }
