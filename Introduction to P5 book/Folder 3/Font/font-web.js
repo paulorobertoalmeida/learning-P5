8658a7da04b16542f20a1d20c9e1b00e324e7717
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-17 10:35:28 +0200

    Different fill & stroke

--- Introduction to P5 book/Folder 3/Font/font-web.js
+++ Introduction to P5 book/Folder 3/Font/font-web.js
@@ -9,3 +9,19 @@ function setup() {
     textSize(28);
     text("Here comes the Cowboy", 25, 60);
   }
+
+
+  //Using Different stroke and fill 
+  function setup() {
+    createCanvas(625, 280);
+    textFont("Source Code Pro");
+    fill(0);
+    stroke(255);
+    
+  }
+  
+  function draw() {
+    background(108);
+    textSize(58);
+    text("Here comes the Cowboy", 25, 60);
+  }
