b1c1af6a78d8f37f4a5561fc8182bd550e73a83f
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-18 12:33:22 +0200

    Random Graphics: exemple using createGraphic

---
+++ exemple1/random-graphics.js
@@ -0,0 +1,32 @@
+//Graphics
+
+let x = 200;
+let y = 200;
+let extraCanvas;
+
+function setup() {
+  createCanvas(400, 400);
+  extraCanvas = createGraphics(400, 400);
+  extraCanvas.background(255, 0, 0);
+  extraCanvas.clear();
+  background(0);
+}
+
+function draw() {
+  
+  x += random(-5, 5);
+  y += random(-5, 5);
+  
+    extraCanvas.fill(255, 150);
+    extraCanvas.noStroke();
+  let starX = random(width);
+  let starY = random(height);
+    extraCanvas.ellipse(starX, starY, 10, 10);
+ 
+  
+  image(extraCanvas, 0, 0);
+  fill(255, 0, 0);
+  stroke(255);
+  rectMode(CENTER);
+  rect(x, y, 20, 20);
+}
