dfc8cfb535ea0f66de788e534eea272658ab6b4d
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-18 10:13:08 +0200

    Frames per Second: pacman exemple returning to the screen

--- Introduction to P5 book/Folder 3/Robot 4/Frame Rate/pacman.js
+++ Introduction to P5 book/Folder 3/Robot 4/Frame Rate/pacman.js
@@ -16,3 +16,25 @@ function draw() {
   noStroke();
   arc(x, 60, radio, radio, 0.52, 5.76);
 }
+
+//PacMan exemple Returning to the screen
+
+let radio = 40;
+let x = -radio;
+let speed = 0.5;
+
+function setup() {
+  createCanvas(240, 120);
+  ellipse(RADIUS);
+}
+
+function draw() {
+  background(50);
+  x += speed;
+  if ( x > width + radio) {
+    x = -radio;
+  }
+  fill(255,255,0);
+  noStroke();
+  arc(x, 60, radio, radio, 0.52, 5.76);
+}
