ab886e44a61ca336ec653a71fd8d2910d893dd6c
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-18 10:43:16 +0200

    Random Two: mouse moving acording to mouse position

--- Introduction to P5 book/Folder 3/Robot 4/Randomness/random-one.js
+++ Introduction to P5 book/Folder 3/Robot 4/Randomness/random-one.js
@@ -9,3 +9,21 @@ function setup() {
     let r = random(0, mouseX);
     print(r);
   }
+
+//Randomness #22
+// Lines moving acording to mouse positions
+
+function setup() {
+    createCanvas(400, 120);
+  }
+  
+  function draw() {
+    background(220);
+    
+    for (let x = 20; x < width; x +=20) {
+      let mx = mouseX /10;
+      let distanceA = random(-mx, mx);
+      let distanceB = random(-mx, mx);
+      line(x + distanceA, 20, x - distanceB, 100);
+    }
+  }
