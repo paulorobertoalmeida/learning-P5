bad5126234795cc7d0c2c4f8c5d48852f3779fa2
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-19 10:48:10 +0200

    SIn & Cos: Circular moviment1

---
+++ Introduction to P5 book/Folder 3/Sin & Cos/circular-moviment.js
@@ -0,0 +1,18 @@
+//Circular Moviment
+
+let angle = 0.0;
+let distance = 200;
+let scale = 100;
+let speed = 0.9;
+
+function setup() {
+  createCanvas(400, 400);
+  background(153, 0, 73);
+}
+
+function draw() {
+  let x = distance + cos(angle) * scale;
+  let y = distance + sin(angle) * scale;
+  ellipse(x, y, 120, 120);
+  angle += speed
+}
