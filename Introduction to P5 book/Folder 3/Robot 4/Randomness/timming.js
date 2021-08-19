7f19087b780823bcaa403d7e3faf5a42b18b3a95
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-19 10:20:29 +0200

    Timming: first exemple 2

--- Introduction to P5 book/Folder 3/Robot 4/Randomness/timming.js
+++ Introduction to P5 book/Folder 3/Robot 4/Randomness/timming.js
@@ -8,3 +8,24 @@ function setup() {
     let timming = millis();
     print(timming);
   }
+
+  //Timming Exemple #2
+
+let moment1 = 2000;
+let moment2 = 4000;
+let x = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  actualMoment = millis();
+  background(50);
+  if (actualMoment < moment2) {
+    x -= 0.5;
+  } else if (actualMoment > moment1) {
+    x += 2;
+  }
+  ellipse(x, 60, 90, 90);
+}
