8e5f7a33cd9d54b53e8ebaa7396f7d5db887de12
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-19 18:45:51 +0200

    Functions: Dices Exemple 2

---
+++ Introduction to P5 book/Folder 4/functions/dices-exemple2.js
@@ -0,0 +1,18 @@
+//Another way to throw dices
+
+function setup() {
+    print("Ready to throw dices");
+    let d1 = 1 + int(random(20));
+    print("Throwing..." + d1);
+    let d2 = 1 + int(random(20));
+    print("throwing..." + d2);
+    let d3 = 1 + int(random(6));
+    print("Throwing..." + d3);
+    
+    print("Done.");
+    
+  }
+  
+  function draw() {
+    background(220);
+  }
