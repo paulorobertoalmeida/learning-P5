5159f1db91451e83a6ff11d711a152a587d38101
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-20 10:33:46 +0200

    Functions: Drawing an owl Multiple times with colors

--- Introduction to P5 book/Folder 4/functions/owl-multiple-times.js
+++ Introduction to P5 book/Folder 4/functions/owl-multiple-times.js
@@ -31,3 +31,41 @@ function setup() {
     pop();
   }  
   
+
+  //Drawing an Owl Repetition with colors
+
+function setup() {
+    createCanvas(480, 120);
+  }
+  
+  function draw() {
+    background(204);
+    randomSeed(0);
+    
+    for (let i = 0; i < width + 40; i += 40) {
+      let grey = int(random(0, 102));
+      let scale = random(0.25, 1.0);
+      owl(i, 110, grey, scale);
+    }
+    
+  }
+  
+  function owl(x, y, g, s) {
+    push();
+    translate(x, y);
+    scale(s);
+    stroke(g);
+    strokeWeight(70);
+    line(0, -35, 0, -65);
+    noStroke();
+    fill(250-g);
+    ellipse(-17.5, -65, 35, 35);
+    ellipse(17.5, -65, 35, 35);
+    arc(0, -65, 70, 70, 0, PI);
+    fill(g);
+    ellipse(-14, -65, 8, 8);
+    ellipse(14, -65, 8, 8);
+    quad(0, -58, 4, -51, 0, -44, -4, -51);
+    pop();
+  }  
+  
