2b7078bf06d98fd8ea689fb12eeba1e6ca7b4d99
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-21 11:36:27 +0200

    Constructor 2

--- Introduction to P5 book/Folder 4/functions/constructor-this.js
+++ Introduction to P5 book/Folder 4/functions/constructor-this.js
@@ -1,23 +1,30 @@
+// Creating an object
+
+var bug;
+
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(600, 600);
+  background(47,79,79);
+  bug = new JitterBug(width/2, height/2, 30);
+}
+
+function draw() {
+  bug.move();
+  bug.show();
+}
   
-  function draw() {
-    background(220);
-  }
-  
-  function JitterBug(timeX, timeY, timeDiameter) {
-    this.x = timex;
+function JitterBug(timeX, timeY, timeDiameter) {
+    this.x = timeX;
     this.y = timeY;
     this.diameter = timeDiameter
-    this.speed = 0.5;
+    this.speed = 2;
     
     this.move = function(){
       this.x += random(-this.speed, this.speed);
       this.y += random(-this.speed, this.speed);
     };
+    
     this.show = function(){
       ellipse(this.x, this.y, this.diameter, this.diameter);
     };
   }
-  
