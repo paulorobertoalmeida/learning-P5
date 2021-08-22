c30a02fdc8f89fdaaeb2672ed91c4f1735b6cb15
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-22 18:04:15 +0200

    Multiple objects part 2

--- Introduction to P5 book/Folder 4/functions/multiple-objects.js
+++ Introduction to P5 book/Folder 4/functions/multiple-objects.js
@@ -35,3 +35,33 @@ function JitterBug(timeX, timeY, timeDiameter) {
       ellipse(this.x, this.y, this.diameter, this.diameter);
     };
   }
+
+
+  //Multiple Objects
+let jit;
+let bug;
+let jit2;
+let bug2;
+
+function setup() {
+  createCanvas(600, 600);
+  background(112,128,144);
+  jit = new JitterBug(width * 0.33, height/2, 40);
+  bug = new JitterBug(width * 0.66, height/2, 20);
+  jit2 = new JitterBug(width * 0.76, height/2, 40);
+  bug2 = new JitterBug(width * 0.11, height/2, 20);
+}
+
+function draw() {
+  
+  
+  jit.move();
+  jit.show();
+  bug.move();
+  bug.show();
+  jit2.move();
+  jit2.show();
+  bug2.move();
+  bug2.show();
+  
+}
