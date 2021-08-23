8c7b407f2cc176d025330eef1f7606647f0d9de6
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-23 10:32:36 +0200

    Arrays: Arcs Exemple 3

--- Introduction to P5 book/Folder 4/Arrays/arcs.js
+++ Introduction to P5 book/Folder 4/Arrays/arcs.js
@@ -45,3 +45,28 @@ function draw() {
   arc(x5, 1000, 20, 20, 0.52, 5.76);
   
 }
+
+
+// Arrays Ex 3 - Using array
+
+let x = [];
+
+
+function setup() {
+  createCanvas(600, 600);
+  noStroke();
+  fill(255, 200);
+  for (let i = 0; i < 3000; i++ ) {
+    x[i] = random(-1000, 200);
+  }
+}
+
+function draw() {
+  background(50);
+  for (let i = 0; i < x.length; i++) {
+    x[i] += 0.5;
+    let y = i * 0.4;
+    fill(255, 255, 0);
+    arc(x[i], y, 12, 12, 0.52, 5.76);
+  }
+}
