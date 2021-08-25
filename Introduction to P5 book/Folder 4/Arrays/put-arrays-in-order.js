de7b5667cc961cff3ec9fd664f8b90242b83186d
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-25 12:42:29 +0200

    Data: Putting Arrays in Order - Sort

--- Introduction to P5 book/Folder 4/Arrays/put-arrays-in-order.js
+++ Introduction to P5 book/Folder 4/Arrays/put-arrays-in-order.js
@@ -8,3 +8,14 @@ console.log(nums);
 /* 
 [ 5, 4, 3, 2, 1 ]
 */
+
+// Put Array Elements in Order
+// sort()
+
+let names = ["David", "Mike", "Cynthia", "Clayton", "Bryan", "Raymond"];
+names.sort();
+console.log(names)
+
+/* 
+[ 'Bryan', 'Clayton', 'Cynthia', 'David', 'Mike', 'Raymond' ]
+*/
