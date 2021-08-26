6714913974e5b0bf10af0ec06459b8f32881bdf1
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-26 10:38:26 +0200

    P5 Book: Reading JSON Data Exemple 2

--- Introduction to P5 book/Folder 4/Data/json-data.js
+++ Introduction to P5 book/Folder 4/Data/json-data.js
@@ -18,3 +18,51 @@ function setup() {
 function draw() {
   background(220);
 }
+
+
+// Reading JSON Exemple 2
+
+let movies = [];
+let movieData;
+
+function preload() {
+  movieData = loadJSON("film.json");
+}
+
+function setup() {
+  createCanvas(480, 120);
+  for ( let i = 0; i < movieData.length; i++) {
+    let o = movieData[i];
+    movies[i] = new Movie(o)
+  }
+  noStroke();
+
+  
+}
+
+function draw() {
+  background(220);
+  for (let i = 0; i < movies.lenght; i++) {
+    let x = i*32 + 32;
+    movies[i].show(x, 105);
+  
+  }
+}
+
+function Film(f) {
+  this.title = ftitle;
+  this.dir = f.director;
+  this.period = f. year;
+  this.rate = f.rating;
+  
+  this.show = function(x, y) {
+    let rantingsGrey = map(this.rating, 6.5, 8.1, 102, 255);
+    
+    push();
+    translate(x, y);
+    rotate(QUARTER_PI);
+    fill(ratingGrey);
+    text(this.title, 0, 0);
+    pop();
+  }
+}
