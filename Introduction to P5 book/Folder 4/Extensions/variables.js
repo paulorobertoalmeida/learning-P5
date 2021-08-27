143896b89be8b39030aee159bf0cebb274e488cc
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-27 10:57:02 +0200

    Variables: Exemple 1

---
+++ Introduction to P5 book/Folder 4/Extensions/variables.js
@@ -0,0 +1,16 @@
+//Global and Local Variables
+
+let i = 12
+
+function setup() {
+  createCanvas(400, 400);
+  let i = 24
+  print(i);
+  //24
+}
+
+function draw() {
+  background(220);
+  print(i);
+  //12
+}
